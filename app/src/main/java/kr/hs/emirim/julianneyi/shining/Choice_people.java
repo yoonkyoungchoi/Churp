ccd4d542b31221bd365dde41e187e16d3990ab8a
yoonkyoungchoi <s2019w40@e-mirim.hs.kr>
2020-11-02 00:51:13 +0900

    메인 페이지 처음 수정본 (#1)

---
+++ app/src/main/java/kr/hs/emirim/julianneyi/shining/Choice_people.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.julianneyi.shining;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Choice_people extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choice_people);
+    }
+}
--- app/src/main/java/kr/hs/emirim/julianneyi/shining/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/julianneyi/shining/MainActivity.java
@@ -2,7 +2,10 @@ package kr.hs.emirim.julianneyi.shining;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ///ioooouioiuououooiuio
+
+
+
+        Button choicebtn = (Button)findViewById(R.id.choicebtn);
+        choicebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Mainmenu.class);
+                startActivity(intent);
+                startActivity(new Intent(MainActivity.this, Mainmenu.class));
+                overridePendingTransition(R.anim.action_left, R.anim.action_left);
+            }
+        });
     }
 }
