7f027b717081c80b9d88504f49193392946a906b
chohyeryung <s2019w38@e-mirim.hs.kr>
2020-11-17 17:55:59 +0900

    더치페이 디자인, 기능 구현 commit2 #7

--- app/src/main/java/kr/hs/emirim/julianneyi/shining/DutchActivity.java
+++ app/src/main/java/kr/hs/emirim/julianneyi/shining/DutchActivity.java
@@ -3,15 +3,26 @@ package kr.hs.emirim.julianneyi.shining;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class DutchActivity extends AppCompatActivity {
 
     private EditText et_people_num;
     private EditText et_total_money;
     private Button goDutch;
+    private int people_num;
+    private int total_money;
+    private TextView tv_money;
+    private LinearLayout result;
+    private CustomDialog customDialog;
+    private int one_money;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +33,34 @@ public class DutchActivity extends AppCompatActivity {
         et_total_money=(EditText)findViewById(R.id.et_total_money);
         goDutch=findViewById(R.id.goDutch);
 
+        //final Handler timer = new Handler(); //Handler 생성
+        customDialog = new CustomDialog(DutchActivity.this);
         goDutch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+//                timer.postDelayed(new Runnable(){ //2초후 쓰레드를 생성하는 postDelayed 메소드
+//                    public void run(){
+//
+//                    }
+//
+//                }, 2000); //
+
+                customDialog.show();
+                ShowResult();
             }
         });
+
+    }
+
+    private void ShowResult() {
+        people_num = Integer.parseInt(et_people_num.getText().toString());
+        total_money = Integer.parseInt(et_total_money.getText().toString());
+        customDialog.dismiss();
+
+        one_money=people_num/total_money;
+        tv_money=findViewById(R.id.tv_money);
+        tv_money.setText(one_money+"원");
+
+        result.setVisibility(View.VISIBLE);
     }
 }
