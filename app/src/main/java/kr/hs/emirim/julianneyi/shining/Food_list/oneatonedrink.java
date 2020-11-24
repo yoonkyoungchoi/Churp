3b3eaf68fb8027aae5d27cfe0bece842f0bb0ab5
yoonkyoungchoi <s2019w40@e-mirim.hs.kr>
2020-11-24 19:20:50 +0900

    메인 메뉴 페이지#5

---
+++ app/src/main/java/kr/hs/emirim/julianneyi/shining/Food_list/oneatonedrink.java
@@ -0,0 +1,16 @@
+package kr.hs.emirim.julianneyi.shining.Food_list;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kr.hs.emirim.julianneyi.shining.R;
+
+public class oneatonedrink extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oneatonedrink);
+    }
+}
