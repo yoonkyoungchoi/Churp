6a4a294d7b92ade3c295b1ce7cebd8b5270bd41a
chohyeryung <s2019w38@e-mirim.hs.kr>
2020-11-26 22:49:32 +0900

    back btn commit

--- app/src/main/java/kr/hs/emirim/julianneyi/shining/Food_list/NospicyActivity.java
+++ app/src/main/java/kr/hs/emirim/julianneyi/shining/Food_list/NospicyActivity.java
@@ -3,14 +3,27 @@ package kr.hs.emirim.julianneyi.shining.Food_list;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import kr.hs.emirim.julianneyi.shining.R;
 
 public class NospicyActivity extends AppCompatActivity {
 
+    private Button btn_back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nospicy);
+
+        btn_back=findViewById(R.id.btn_back);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
